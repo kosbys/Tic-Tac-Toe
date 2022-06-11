502624503d389ff322979850c2642d3ab10a571e
kosbys <kosmicabysslord@protonmail.com>
2022-06-11 20:18:53 +0300

    Add basic grid to HTML

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 const gameBoard = (() => {
+    const board = document.querySelector('.board');
     const boardSize = 3;
 
-    const board = Array(boardSize)
+    const boardGrid = Array(boardSize)
         .fill()
-        .map(() => Array(boardSize).fill(0));
-    return { board };
+        .map(() => Array(boardSize).fill(''));
+
+    const createBoard = (() => {
+        boardGrid.forEach((row, i) => {
+            row.forEach((cell, j) => {
+                div = document.createElement('div');
+                div.classList.add('cell');
+                div.id = `#${i + j + 1}`;
+                div.innerText = cell;
+
+                board.appendChild(div);
+            });
+        });
+    })();
+
+    return { boardGrid };
 })();
 
-const playerFactory = (name) => {
+const displayController = (() => {})();
+
+const playerFactory = (name, marker) => {
     const placeMarker = (i, j) => {
-        gameBoard.board[i][j] = 'O';
+        gameBoard.boardGrid[i][j] = marker;
     };
+
     return { placeMarker, name };
 };
 
-let me = playerFactory('a');
+let me = playerFactory('a', 'O');
 
 me.placeMarker(1, 1);
 
